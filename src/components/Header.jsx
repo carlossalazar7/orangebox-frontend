f6536aea4cf3b536c0c5ad650f35de19bd2cb259
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2025-06-03 13:46:13 -0600

    mejoras visuales

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import { Link, useLocation } from 'react-router-dom';
 
 export default function Header() {
     const location = useLocation();
-    const [dark, setDark] = useState(() => localStorage.getItem('theme') === 'dark');
+    const [dark, setDark] = useState(() => {
+        const stored = localStorage.getItem('theme');
+        return stored ? stored === 'dark' : true;
+    });
 
     useEffect(() => {
         if (dark) {
@@ -23,10 +26,10 @@ export default function Header() {
                         <img src="https://img.icons8.com/color/48/orange.png" alt="Logo" className="w-10 h-10 cursor-pointer" />
                     </Link>
                 </div>
-                <div>
+                <div className='container-header flex items-center justify-between w-full'>
                     <button
                         onClick={() => setDark(!dark)}
-                        className="ml-8 px-3 py-1 rounded bg-white/20 hover:bg-white/40 text-white font-semibold transition"
+                        className="ml-2 px-3 py-1 rounded bg-white/20 hover:bg-white/40 text-white font-semibold transition border-2 border-white focus:ring-2 focus:ring-orange-300 border-theme"
                         title="Cambiar tema"
                     >
                         {dark ? '🌙' : '☀️'}
